ac0d354011fc4a920188d7f26878df5949898b4c
tattuaaa <tatupann@gmail.com>
2020-03-18 19:14:23 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 'use strict';
+const momo = new Map();
+momo.set(0, 0);
+momo.set(1, 1);
+
+function fib(n){
+    if(momo.has(n)){
+        return momo.get(n);
+    }
+    const value = fib(n - 1) + fib(n - 2);
+    momo.set(n, value);
+    return value;
+}/*
 function fib(n) {
     if (n === 0) {
         return 0;
@@ -6,7 +18,7 @@ function fib(n) {
         return 1;
     }
     return fib(n - 1) + fib(n - 2);
-}
+}*/
 const length = 40;
 for (let i = 0; i <= length; i++){
     console.log(fib(i));
